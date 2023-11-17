18d0906dd6701b07e06429e3a14825a9f35ed64e
Zhang Yu <yzhang5@insa-20935>
2023-11-17 17:42:02 +0100

    tttttttttest

--- chatsysteme/src/main/java/protocols/UDP.java
+++ chatsysteme/src/main/java/protocols/UDP.java
@@ -60,5 +60,30 @@ public class UDP {
     public List<contact> getContacts() {
         return new ArrayList<>(contacts);
     }
+
+    // Inclure le code existant de UdpDiscovery, puis ajouter :
+
+    public void listenForRequestsAndRespond() {
+        new Thread(() -> {
+            try {
+                DatagramSocket responseSocket = new DatagramSocket();
+                while (true) {
+                    DatagramPacket requestPacket = new DatagramPacket(new byte[512], 512);
+                    socket.receive(requestPacket);
+
+                    String request = new String(requestPacket.getData(), 0, requestPacket.getLength());
+                    if (request.equals("DISCOVER_REQUEST")) {
+                        String responseMessage = "DISCOVER_RESPONSE:YourName";
+                        byte[] responseData = responseMessage.getBytes();
+                        DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, requestPacket.getAddress(), requestPacket.getPort());
+                        responseSocket.send(responsePacket);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
 }
 
--- chatsysteme/src/test/java/testsession1.java
+++ chatsysteme/src/test/java/testsession1.java
@@ -4,24 +4,12 @@ import model.contact.contact;
 import java.util.List;
 
 public class testsession1 {
-    public static void main(String[] args) {
 
-        UDP udpDiscovery = new UDP(8888);
-
-        udpDiscovery.sendDiscoveryRequest();
-
-        try {
-            Thread.sleep(10000); // Attendre 10 secondes
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        public static void main(String[] args) {
+            UDP udpDiscovery = new UDP(8888);
+            udpDiscovery.listenForRequestsAndRespond();
         }
 
-        List<contact> discoveredContacts = udpDiscovery.getContacts();
-        for (contact contact : discoveredContacts) {
-            System.out.println("Découvert : " + contact);
 
-
-        }
-    }
 }
 
