c439beb4baa466acdd37428bd10b18d36216f8cf
Zhang Yu <yzhang5@insa-20930>
2024-01-12 11:26:10 +0100

    update bdd

--- src/main/java/view/messagerie.java
+++ src/main/java/view/messagerie.java
@@ -2,8 +2,11 @@ package view;
 
 import java.awt.BorderLayout;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,8 +15,8 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import controller.controllerMessage;
-import model.user;
 import model.BaseDeDonnee.dataMessage;
+import model.user;
 import model.contact.contact;
 
 public class messagerie {
@@ -29,12 +32,22 @@ public class messagerie {
         System.out.println(username +"envoyer à "+friendname);
         conMsg.getTcpr().setMessageListener(message->{
             try {
+                String[] parts = message.split("_");
+                if (parts.length >= 4) {
+                    String sender = parts[0];
+                    String receiver = parts[1];
+                    Date time = convertir_string_date(parts[2]);
+                    String msgContent = parts[3];
+                    this.conMsg.getBdd().addmessageData(sender, receiver, time, msgContent);
 				Thread.sleep(500);
-			} catch (InterruptedException e) {
+                updateHistory(friendname,username);}
+                else{System.out.println("[view] messagerie: recever un error message");}
+            } catch (InterruptedException e) {
                 System.out.println("[view] messagerie: erreur de sleep");
 				e.printStackTrace();
 			}
-            updateHistory(friendname,username);
+            
+            
         });
         frame = new JFrame(currentContact.getUserName());
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -59,7 +72,7 @@ public class messagerie {
                         Date now = new Date(); // Obtient la date et l'heure actuelles
                         String msg = messageField.getText();
                         messageField.setText("");
-                        conMsg.envoyermsg(msg,currentContact.getUserIP(),now );
+                        conMsg.envoyermsg(username+"_"+friendname+"_"+now+"_"+msg,currentContact.getUserIP(),now );
                         //appendToChatHistory("["+now+"]"+user.getUserlocal().getUserName()+": "+ msg);
                         updateHistory(username,friendname);
                     } catch (IOException e1) {
@@ -85,7 +98,15 @@ public class messagerie {
             appendToChatHistory("["+data.time()+"]"+data.sender()+": "+data.message());
         }       
     }
-
+    private Date convertir_string_date(String timestamp){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return formatter.parse(timestamp);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     private void appendToChatHistory(String message) {
         chatHistoryArea.append(message + "\n");
     }
